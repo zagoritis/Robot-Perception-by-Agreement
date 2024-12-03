d34b86c989ba57d6210857fa41d6d8d0cb2b769c
Emmanouil Zagoritis <51760742+zagoritis@users.noreply.github.com>
2024-12-03 12:29:47 +0100

    Added more non-verbal cues

--- non_verbal_cues.py
+++ non_verbal_cues.py
@@ -3,31 +3,79 @@ from twisted.internet.defer import inlineCallbacks
 @inlineCallbacks
 def perform_non_verbal_cue(session, action):
     try:
+        #AGREE:
         if action == "nod_agreement":
             yield session.call("rom.actuator.motor.write", frames=[
                 {"time": 500, "data": {"body.head.pitch": 0.2}},
                 {"time": 1000, "data": {"body.head.pitch": 0.0}}
             ])
-        
+            
+        elif action == "double_nod_agreement":
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 300, "data": {"body.head.pitch": 0.1}},
+                {"time": 600, "data": {"body.head.pitch": 0.0}},
+                {"time": 900, "data": {"body.head.pitch": 0.1}},
+                {"time": 1200, "data": {"body.head.pitch": 0.0}}
+            ])
+
         elif action == "nod_slightly":
             yield session.call("rom.actuator.motor.write", frames=[
                 {"time": 500, "data": {"body.head.pitch": 0.05}},
                 {"time": 1000, "data": {"body.head.pitch": 0.0}}
             ])
             
-        elif action == "shake_head":
-            yield session.call("rom.actuator.motor.write", frames=[
-                {"time": 500, "data": {"body.head.yaw": -0.2}},
-                {"time": 1000, "data": {"body.head.yaw": 0.2}},
-                {"time": 1500, "data": {"body.head.yaw": 0.0}}
-            ])
-        
         elif action == "agree_eyes":
             yield session.call("rom.actuator.light.write", frames=[
                 {"time": 100, "data": {"body.head.eyes": [0, 255, 0]}},
                 {"time": 300, "data": {"body.head.eyes": [255, 255, 255]}},
                 {"time": 700, "data": {"body.head.eyes": [0, 255, 0]}},
                 {"time": 1000, "data": {"body.head.eyes": [255, 255, 255]}}
+            ])
+       
+        elif action == "thumbs_up":
+            yield session.call("rom.optional.behavior.play", name="BlocklyArmsForward")
+            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+
+        elif action == "happy":
+            yield session.call("rom.optional.behavior.play", name="BlocklyHappy")
+            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+
+        elif action == "shrug":
+            yield session.call("rom.optional.behavior.play", name="BlocklyShrug")
+            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+            
+        elif action == "clap":
+            yield session.call("rom.optional.behavior.play", name="BlocklyApplause")
+            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+            
+        #DISAGREE:
+        elif action == "shake_head":
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 500, "data": {"body.head.yaw": -0.2}},
+                {"time": 1000, "data": {"body.head.yaw": 0.2}},
+                {"time": 1500, "data": {"body.head.yaw": 0.0}}
+            ])
+            
+        elif action == "shake_head_fast":
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 200, "data": {"body.head.yaw": -0.3}},
+                {"time": 400, "data": {"body.head.yaw": 0.3}},
+                {"time": 600, "data": {"body.head.yaw": -0.3}},
+                {"time": 800, "data": {"body.head.yaw": 0.3}},
+                {"time": 1000, "data": {"body.head.yaw": 0.0}}
+            ])
+
+        elif action == "shake_head_low":
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 500, "data": {"body.head.pitch": 0.2}},
+            ])
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 800, "data": {"body.head.yaw": -0.1}},
+                {"time": 1000, "data": {"body.head.yaw": 0.1}},
+                {"time": 1200, "data": {"body.head.yaw": 0.0}}
+            ])
+            yield session.call("rom.actuator.motor.write", frames=[
+                {"time": 1700, "data": {"body.head.pitch": 0.0}}
             ])
         
         elif action == "disagree_eyes":
@@ -36,34 +84,19 @@ def perform_non_verbal_cue(session, action):
                 {"time": 1000, "data": {"body.head.eyes": [255, 255, 255]}}
             ])
 
-        elif action == "thumbs_up":
-            yield session.call("rom.optional.behavior.play", name="BlocklyArmsForward")
-            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
-        
         elif action == "point":
             yield session.call("rom.optional.behavior.play", name="BlocklyRightArmForward")
             yield session.call("rom.optional.behavior.play", name="BlocklyRightHandOpen")
             yield session.call("rom.optional.behavior.play", name="BlocklyStand")
-        
+
         elif action == "shrug":
             yield session.call("rom.optional.behavior.play", name="BlocklyShrug")
             yield session.call("rom.optional.behavior.play", name="BlocklyStand")
-            
-        elif action == "clap":
-            yield session.call("rom.optional.behavior.play", name="BlocklyApplause")
-            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
-        
-        elif action == "look_around":
-            yield session.call("rom.actuator.motor.write", frames=[
-                {"time": 1000, "data": {"body.head.yaw": 0.8}},
-                {"time": 500, "data": {"body.head.pitch": -0.1}},
-                {"time": 500, "data": {"body.head.pitch": 0.0}},
-                {"time": 4000, "data": {"body.head.yaw": -0.8}},
-                {"time": 500, "data": {"body.head.pitch": 0.1}},
-                {"time": 500, "data": {"body.head.pitch": 0.0}},
-                {"time": 7000, "data": {"body.head.yaw": 0.0}}
-            ])
 
+        elif action == "fear":
+            yield session.call("rom.optional.behavior.play", name="BlocklyFearUp")
+            yield session.call("rom.optional.behavior.play", name="BlocklyStand")
+    
         else:
             print("No non-verbal action to perform.")
         
